f394b5b3110ccb485da279e8f01bef5af6512b92
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-05-15 23:04:18 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,147 +1,137 @@
-import discord
-from discord.ext import commands
-import datetime
-import time
 import asyncio
-import os
 
-import httplib2
-from urllib.parse import urlencode, quote # For URL creation
-from urllib import parse, request
-import re
-mary_host = "84.27.169.137"
-mary_port = "6754"
-bot = commands.Bot(command_prefix=os.environ['PREFIX'], description="This is a Helper Bot")
+import discord
+import youtube_dl
 
+from discord.ext import commands
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
 
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-@bot.command(pass_context=True)
-async def radio(ctx):
-         """play the radio"""
-         voice = await ctx.author.voice.channel.connect()
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
 
-         voice.play(discord.FFmpegPCMAudio(os.environ['RADIO_LINK']))
-         await ctx.send('playing the radio now, to stop it u need to dissconnect the bot from the vc yourself')
+        self.data = data
 
-@bot.command(pass_context=True)
-async def darkpony(ctx):
-         """fuck with dark"""
-         voice = await ctx.author.voice.channel.connect()
+        self.title = data.get('title')
+        self.url = data.get('url')
 
-         voice.play(discord.FFmpegPCMAudio('dark.wav'))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-         
-@bot.command(pass_context=True)
-async def sfx1(ctx):
-         """play sfx1"""
-         voice = await ctx.author.voice.channel.connect()
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
-         voice.play(discord.FFmpegPCMAudio(os.environ['SFX1']))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-         
-         
-@bot.command(pass_context=True)
-async def sfx2(ctx):
-         """play sfx2"""
-         voice = await ctx.author.voice.channel.connect()
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
 
-         voice.play(discord.FFmpegPCMAudio(os.environ['SFX2']))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-         
-         
-        
-@bot.command(pass_context=True)
-async def sfx3(ctx):
-         """play sfx3"""
-         voice = await ctx.author.voice.channel.connect()
-
-         voice.play(discord.FFmpegPCMAudio(os.environ['SFX3']))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-         
-         
-@bot.command(pass_context=True)
-async def sfx4(ctx):
-         """play sfx4"""
-         voice = await ctx.author.voice.channel.connect()
-
-         voice.play(discord.FFmpegPCMAudio(os.environ['SFX4']))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-
-         
-         
-         
-@bot.command(pass_context=True)
-async def ping(ctx):
-         """check if i am still alive"""
-         await ctx.send('i am still alive')
-         
-         
-@bot.command(pass_context=True)
-async def rick(ctx):
-         """get rickrolled"""
-         voice = await ctx.author.voice.channel.connect()
-
-         voice.play(discord.FFmpegPCMAudio('rick.mp3'))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
-         
-         
-
-@bot.command(pass_context=True)
-async def tts(ctx, *, text: str):
-         """text to speech api"""
-         input_text = "{}".format(text)
-        
-         query_hash = {"INPUT_TEXT":input_text,
-                       "INPUT_TYPE":"TEXT", # Input text
-                       "LOCALE":"en_US",
-                       "VOICE":os.environ['MAIN_VOICE'], # Voice informations  (need to be compatible)
-                       "OUTPUT_TYPE":"AUDIO",
-                       "AUDIO":"WAVE", # Audio informations (need both)
-                       }
-         query = urlencode(query_hash)
-         print("query = \"http://%s:%s/process?%s\"" % (mary_host, mary_port, query))
-
-         h_mary = httplib2.Http()
-         resp, content = h_mary.request("http://%s:%s/process?" % (mary_host, mary_port), "POST", query)
-
-         if (resp["content-type"] == "audio/x-wav"):
-
-             # Write the wav file
-             f = open("output_wav.wav", "wb")
-             f.write(content)
-             f.close()
-        
-         voice = await ctx.author.voice.channel.connect()
-
- 
-         voice.play(discord.FFmpegPCMAudio('output_wav.wav'))
-         while voice.is_playing():
-             await asyncio.sleep(.1)
-         await voice.disconnect()
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
 
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
 
- 
+        await channel.connect()
 
-# Events
+    @commands.command()
+    async def play(self, ctx, *, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {query}')
+
+    @commands.command()
+    async def yt(self, ctx, *, url):
+        """Plays from a url (almost anything youtube_dl supports)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    @yt.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='Relatively simple music bot example')
+
 @bot.event
 async def on_ready():
-    print('My Body is Ready')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
-bot.run(os.environ['TOKEN'])
+bot.add_cog(Music(bot))
+bot.run('token')
