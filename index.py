37cc419a640357100fd2c16522b1325a1e55da9d
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-05-16 11:17:59 +0200

    Update index.py

--- index.py
+++ index.py
@@ -62,14 +62,7 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
-
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
-
-        await channel.connect()
+   
         
     @commands.command()
     async def yt(self, ctx, *, url):
