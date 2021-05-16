89500a605f6a0a70819a1e3f74a9797c4c474cd3
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-05-16 09:19:51 +0200

    Update index.py

--- index.py
+++ index.py
@@ -127,9 +127,13 @@ class Music(commands.Cog):
              f.write(content)
              f.close()
             
-         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('output_wav.wav'))
-         ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+         voice = await ctx.author.voice.channel.connect()
 
+ 
+         voice.play(discord.FFmpegPCMAudio('output_wav.wav'))
+         while voice.is_playing():
+             await asyncio.sleep(.1)
+         await voice.disconnect()
         
         
         
